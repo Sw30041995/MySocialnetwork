7f77b3df6a2cd320fb0c088219c168c3e0669765
sw30041995 <sw1225@yandex.ru>
2021-12-10 20:59:04 +0300

    new files

--- src/App.tsx
+++ src/App.tsx
@@ -14,8 +14,8 @@ export const App = () => {
                 <Navbar/>
                 <div className='app-wrapper-content'>
                     <Routes>
-                        <Route path='/profile' element={<Profile/>}/>
-                        <Route path='/dialogs' element={<Dialogs/>}/>
+                        <Route path='/profile/*' element={<Profile/>}/>
+                        <Route path='/dialogs/*' element={<Dialogs/>}/>
                     </Routes>
                 </div>
             </div>
