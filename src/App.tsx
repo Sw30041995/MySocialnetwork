78538aa6b1cec8b4465e02973c6020e20e34392c
sw30041995 <sw1225@yandex.ru>
2021-12-12 17:26:39 +0300

    new files

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,10 @@ import {Profile} from "./components/Profile/Profile";
 import {Dialogs} from "./components/Dialogs/Dialogs";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import {state} from "./redux/state";
+import {News} from './components/News/News';
+import {Music} from './components/Music/Music';
+import {Settings} from './components/Settings/Settings';
+
 
 export const App = () => {
     return (
@@ -17,7 +21,9 @@ export const App = () => {
                     <Routes>
                         <Route path='/profile/*' element={<Profile profilePage={state.profilePage}/>}/>
                         <Route path='/dialogs/*' element={<Dialogs dialogsPage={state.dialogsPage}/>}/>
-
+                        <Route path='/news/*' element={<News/>}/>
+                        <Route path='/music/*' element={<Music/>}/>
+                        <Route path='/settings/*' element={<Settings/>}/>
                     </Routes>
                 </div>
             </div>
